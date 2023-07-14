f12031d0a5f93043aa425be07f8bab1d8d39b832
pstojkov <petar.stojkovic@cern.ch>
2023-07-14 16:13:08 +0200

    Hopefully it will work, updated python script to return name of user and owner

--- PowerShellScripts/SOAPNetworkService.py
+++ PowerShellScripts/SOAPNetworkService.py
@@ -11,11 +11,6 @@ url = 'https://network.cern.ch/sc/soap/soap.fcgi?v=6&WSDL'
 imp = Import('http://schemas.xmlsoap.org/soap/encoding/')
 doc = ImportDoctor(imp)
 client = Client(url, doctor=doc, cache=None)
-print "======"
-print sys.argv[1]
-print sys.argv[2]
-print sys.argv[3]
-print "======="
 # Authentication
 username = sys.argv[2] if len(sys.argv) > 2 else exit("Please specify the username")
 password = sys.argv[3] if len(sys.argv) > 3 else exit("Please specify the password")
@@ -26,7 +21,6 @@ client.set_options(soapheaders=authenticationHeader)
 # Calling getDeviceInfo
 deviceName = sys.argv[1] if len(sys.argv) > 1 else exit("Please specify the set name")
 result = client.service.getDeviceInfo(deviceName)
-print result.ResponsiblePerson.Email
 # Define ldapsearch command
 ldapsearch_base_cmd = 'ldapsearch -z 0 -E pr=1000/noprompt -LLL -x -h "xldap.cern.ch" -b "OU=Users,OU=Organic Units,DC=cern,DC=ch" "(&(objectClass=user)(mail=%s))" cn | grep \'^cn: \' | sed \'s/^cn: //\''
 
